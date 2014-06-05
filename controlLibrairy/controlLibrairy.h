101144b625368aa517affaabd79a4b1646fef8f5
Lagspoon <lagspoon@gmail.com>
2014-06-05 17:01:48 +0200

    Initial Commit

---
+++ controlLibrairy/controlLibrairy.h
@@ -0,0 +1,13 @@
+//
+//  controlLibrairy.h
+//  controlLibrairy
+//
+//  Created by Olivier Delecueillerie on 05/06/2014.
+//  Copyright (c) 2014 lagspoon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface controlLibrairy : NSObject
+
+@end
---
+++ controlLibrairy/controlLibrairy.m
@@ -0,0 +1,13 @@
+//
+//  controlLibrairy.m
+//  controlLibrairy
+//
+//  Created by Olivier Delecueillerie on 05/06/2014.
+//  Copyright (c) 2014 lagspoon. All rights reserved.
+//
+
+#import "controlLibrairy.h"
+
+@implementation controlLibrairy
+
+@end
